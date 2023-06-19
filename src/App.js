fe04036d8e09da487fbeb39727d2551e84ac6145
Jory Roberts <robertsjory13@gmail.com>
2023-06-19 09:45:44 -0500

    creates components folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
 import { useEffect, useState } from 'react';
+import Header from './components/Header';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -17,22 +16,7 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>Phase 2 Project</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+	return (
+		<header>
+			<h1>
+				This is my Header
+			</h1>
+		</header>
+	)
+}
+export default Header
